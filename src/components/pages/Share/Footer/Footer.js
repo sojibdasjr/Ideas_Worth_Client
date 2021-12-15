6395689448e3480c7fcdd79cbc7ad734e44355a5
sojibdasjr <sojibdas47@gmail.com>
2021-12-15 16:40:08 +0600

    added footer

--- src/components/pages/Share/Footer/Footer.js
+++ src/components/pages/Share/Footer/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { faMapMarkerAlt, faEnvelope, faLocationArrow } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Footer.css'
+import { faFacebook, faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
     return (
-        <div className='footer_div'>
+        <div className='footer_div mt-5'>
             <div className='row  m-2'>
                 
                 <div className='col-12 col-md-4 col-lg-3'>
@@ -39,13 +42,38 @@ const Footer = () => {
                     </div>
                 </div>
                 <div className='col-12 col-md-4 col-lg-3'>
-                    <div>
-                        
-                        
+                    <div className='row'>
+                    <div className='col-12 col-md-6 col-lg-6'>
+                        <h5 className='text-center'>GET IN TOUCH</h5>
+                        <div className='d-flex align-items-center'>
+                            <FontAwesomeIcon  icon={faMapMarkerAlt} className='fs-4'/>
+                            <div className='ms-3'>
+                                <small>PO Box 97845 State  st. 557.Los Angles, California US</small>
+                            </div>
+                        </div>
+                        <div className='d-flex align-items-center mt-2'>
+                            <FontAwesomeIcon icon={faEnvelope}/>
+                            <small className='ms-3'>contact.ideasworth@spreading.com</small>
+                        </div>
+                        <div className='d-flex align-items-center mt-2'>
+                            <FontAwesomeIcon icon={faLocationArrow}/>
+                            <small className='ms-3'>apply@ed.com</small>
+                        </div>
+                    </div>
+                    <div className='col-12 col-md-6 col-lg-6'>
+                        <h5>SOCIAL MEDIA</h5>
+                        <div>
+                            <FontAwesomeIcon className='me-2 social_icon' icon={faFacebook}/>
+                            <FontAwesomeIcon className='me-2 social_icon' icon={faInstagram}/>
+                            <FontAwesomeIcon className='me-2 social_icon' icon={faLinkedin}/>
+                            <FontAwesomeIcon className='me-2 social_icon' icon={faTwitter}/>
+                        </div>
+                    </div>
                     </div>
                 </div>
                 
             </div>
+            <small>© 2021 Ideas worth spreading. All Rights Reserved.</small>
         </div>
     );
 };
